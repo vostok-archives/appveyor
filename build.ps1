5635ff76c7e70e59f398fe4bea5b38cdc3856683
lapin <lapin@kontur.ru>
2018-03-21 09:01:33 +0400

    added publishing and testing

--- build.ps1
+++ build.ps1
@@ -32,7 +32,7 @@ foreach($csproj in $csprojs)
   $props = $xml.SelectSingleNode("//PropertyGroup")
   if ($props)
   {
-    echo "Generating version $env:appveyor_build_version for $name"
+    Write-Output "Generating version $env:appveyor_build_version for $name"
     $version = $xml.CreateElement("Version")
     $versionText = $xml.CreateTextNode($env:appveyor_build_version)
     $version.AppendChild($versionText)
@@ -46,19 +46,15 @@ $releases = "https://api.github.com/repos/skbkontur/cement/releases"
 Write-Host Determining latest release
 $download = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets[0].browser_download_url
 
-$zip = "cement.zip"
-$dir = "cement"
-
 Write-Host Dowloading latest release
-Invoke-WebRequest $download -Out $zip
+Invoke-WebRequest $download -Out "cement.zip"
 
 Write-Host Extracting release files
-Expand-Archive $zip -Force -DestinationPath $dir
+Expand-Archive "cement.zip" -Force -DestinationPath "cement"
 Set-Location "cement\dotnet"
 & cmd.exe /c install.cmd
 $wc = New-Object System.Net.WebClient
 Invoke-WebRequest "https://raw.githubusercontent.com/vostok/cement-modules/master/settings" -OutFile "$env:USERPROFILE\.cement\settings"
-$wc.DownloadFile("https://raw.githubusercontent.com/vostok/cement-modules/master/settings", "$env:USERPROFILE\.cement\settings")
 [Environment]::SetEnvironmentVariable("cm", "$env:USERPROFILE\bin\cm.cmd", "User")
 $env:cm = "$env:USERPROFILE\bin\cm.cmd"
 
@@ -66,5 +62,45 @@ Set-Location $env:appveyor_build_folder\..
 & $env:cm init
 Set-Location $env:appveyor_build_folder
 & $env:cm update-deps
+if (!$?) {
+    exit 1
+}
 & $env:cm build-deps
-& dotnet restore
+if (!$?) {
+    exit 1
+}
+& $env:cm build
+if (!$?) {
+    exit 1
+}
+
+if ($env:appveyor_repo_branch -eq "master" -and "$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH" -eq "") {
+  $proj="Vostok.$env:APPVEYOR_PROJECT_NAME.csproj"
+  & $env:cm pack $proj
+  if (!$?) {
+      exit 1
+  }
+  $csprojs = $env:appveyor_build_folder | Get-ChildItem -Recurse -Filter "*.csproj"
+  foreach($csproj in $csprojs)
+  {
+    $name = $csproj.BaseName
+    $nupkgs = Get-ChildItem $env:appveyor_build_folder\$name\bin\Release\$name.*.nupkg
+    if ($nupkgs)
+    {
+      $nupkgs | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+    }          
+  }
+}
+
+if (Test-Path "$env:appveyor_build_folder\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests.csproj")
+{
+  dotnet test "$env:appveyor_build_folder\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests.csproj" --logger "trx;LogFileName=tests.trx"
+  if (!$?) {
+      exit 1
+  }
+  if (Test-Path "$env:appveyor_build_folder\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests\TestResults\tests.trx") {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:appveyor_job_id)", "$env:appveyor_build_folder\Vostok.$env:APPVEYOR_PROJECT_NAME.Tests\TestResults\tests.trx")
+  }
+}
+
